379a60c3a4b7e5b76ea12fec9b5d8bfa39abb85f
German <v-german@pulumi.com>
2024-02-14 08:36:56 -0300

    attempt to reuse copy

---
+++ internal/util/copy.go
@@ -0,0 +1,108 @@
+// Copyright 2023, Pulumi Corporation.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package util
+
+func NewCopier[T any](getter func(v *T) any, setter func(o *T, v any)) Copier[T] {
+	return Copier[T]{
+		memo:   map[*T]*T{},
+		getter: getter,
+		setter: setter,
+	}
+}
+
+type Copier[T any] struct {
+	memo   map[*T]*T
+	setter func(o *T, v any)
+	getter func(v *T) any
+}
+
+func (c Copier[T]) Copy(v *T) *T {
+	if v == nil {
+		return nil
+	}
+
+	if copy, ok := c.memo[v]; ok {
+		return copy
+	}
+
+	copy := new(T)
+	c.memo[v] = copy
+
+	var nv any
+	switch vr := c.getter(v).(type) {
+	case []T:
+		a := make([]T, len(vr))
+		for i, v := range vr {
+			a[i] = *c.Copy(&v)
+		}
+		nv = a
+	case map[string]T:
+		m := make(map[string]T, len(vr))
+		for k, v := range vr {
+			m[k] = *c.Copy(&v)
+		}
+		nv = m
+	default:
+		nv = vr
+	}
+
+	c.setter(copy, nv)
+	return copy
+}
+
+// func newCopier() copier {
+// 	return copier{memo: map[*value]*value{}}
+// }
+
+// func (c copier) copy(v *value) *value {
+// 	if v == nil {
+// 		return nil
+// 	}
+
+// 	if copy, ok := c.memo[v]; ok {
+// 		return copy
+// 	}
+
+// 	copy := &value{}
+// 	c.memo[v] = copy
+
+// 	var repr any
+// 	switch vr := v.repr.(type) {
+// 	case []*value:
+// 		a := make([]*value, len(vr))
+// 		for i, v := range vr {
+// 			a[i] = c.copy(v)
+// 		}
+// 		repr = a
+// 	case map[string]*value:
+// 		m := make(map[string]*value, len(vr))
+// 		for k, v := range vr {
+// 			m[k] = c.copy(v)
+// 		}
+// 		repr = m
+// 	default:
+// 		repr = vr
+// 	}
+
+// 	*copy = value{
+// 		def:     v.def,
+// 		base:    c.copy(v.base),
+// 		schema:  v.schema,
+// 		unknown: v.unknown,
+// 		secret:  v.secret,
+// 		repr:    repr,
+// 	}
+// 	return copy
+// }
---
+++ internal/util/copy_test.go
@@ -0,0 +1,61 @@
+// Copyright 2023, Pulumi Corporation.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package util
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func NewValue[T any](v T) PseudoValue {
+	return PseudoValue{Value: v}
+}
+
+type PseudoValue struct {
+	Value any
+}
+
+func TestCopy(t *testing.T) {
+	a := NewValue(map[string]PseudoValue{
+		"b": NewValue(map[string]PseudoValue{
+			"c": NewValue("a.b"),
+		}),
+	})
+
+	na := NewCopier(
+		func(v *PseudoValue) any {
+			return v.Value
+		},
+		func(o *PseudoValue, v any) {
+			o.Value = v
+		},
+	).Copy(&a)
+
+	newElement, ok := na.Value.(map[string]PseudoValue)
+	assert.True(t, ok)
+	newElement, ok = newElement["b"].Value.(map[string]PseudoValue)
+	assert.True(t, ok)
+	newElement["c"] = NewValue("1.2")
+	newElement["d"] = NewValue("3.4")
+
+	element, ok := a.Value.(map[string]PseudoValue)
+	assert.True(t, ok)
+	element, ok = element["b"].Value.(map[string]PseudoValue)
+	assert.Equal(t, "a.b", element["c"].Value.(string))
+
+	_, ok = element["d"]
+	assert.False(t, ok)
+}
